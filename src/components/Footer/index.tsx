0d2401a1eae4ca718e1f2f70fcdb5c83863a5e6f
Tao Zhou <taozhou@umich.edu>
2023-11-09 23:00:35 -0500

    feat: replace default links in the footer with relevant information

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { GithubOutlined } from '@ant-design/icons';
+import { HomeOutlined, LinkedinOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 import { useIntl } from 'umi';
 
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
   const intl = useIntl();
   const defaultMessage = intl.formatMessage({
     id: 'app.copyright.produced',
-    defaultMessage: '蚂蚁集团体验技术部出品',
+    defaultMessage: 'NanoRes LLC',
   });
 
   const currentYear = new Date().getFullYear();
@@ -16,21 +16,15 @@ const Footer: React.FC = () => {
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'home',
+          title: <HomeOutlined />,
+          href: 'https://nanoresolution.com/',
           blankTarget: true,
         },
         {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          key: 'linkedin',
+          title: <LinkedinOutlined />,
+          href: 'https://www.linkedin.com/company/nanores-llc/',
           blankTarget: true,
         },
       ]}
