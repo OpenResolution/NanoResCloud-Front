b8291a4906a4c39eb03e0e202ca5526e3c5f2fce
Tao Zhou <taozhou@umich.edu>
2023-11-29 18:57:40 -0500

    feat: add locale and email validation for login page

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -1,41 +1,16 @@
-import { getFakeCaptcha, login } from '@/services/nanores-cloud/login';
-import { LockOutlined, MobileOutlined, UserOutlined } from '@ant-design/icons';
-import { ProFormCaptcha, ProFormCheckbox, ProFormText } from '@ant-design/pro-components';
+import { login } from '@/services/nanores-cloud/login';
+import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import { ProFormCheckbox, ProFormText } from '@ant-design/pro-components';
 import { FormattedMessage, Link, history, useIntl, useModel } from '@umijs/max';
 import { Divider, Tabs, message } from 'antd';
 import React, { useState } from 'react';
 import { flushSync } from 'react-dom';
 import { AuthPage, ErrorMessage } from '../components/AuthPage';
 
-// const ActionIcons = () => {
-//   const langClassName = useEmotionCss(({ token }) => {
-//     return {
-//       marginLeft: '8px',
-//       color: 'rgba(0, 0, 0, 0.2)',
-//       fontSize: '24px',
-//       verticalAlign: 'middle',
-//       cursor: 'pointer',
-//       transition: 'color 0.3s',
-//       '&:hover': {
-//         color: token.colorPrimaryActive,
-//       },
-//     };
-//   });
-
-//   return (
-//     <>
-//       <AlipayCircleOutlined key="AlipayCircleOutlined" className={langClassName} />
-//       <TaobaoCircleOutlined key="TaobaoCircleOutlined" className={langClassName} />
-//       <WeiboCircleOutlined key="WeiboCircleOutlined" className={langClassName} />
-//     </>
-//   );
-// };
-
 const Login: React.FC = () => {
-  const [userLoginState, setUserLoginState] = useState<API.LoginResult>({});
-  const [type, setType] = useState<string>('account');
-  const { initialState, setInitialState } = useModel('@@initialState');
   const intl = useIntl();
+  const [userLoginState, setUserLoginState] = useState<API.LoginResult>({});
+  const { initialState, setInitialState } = useModel('@@initialState');
 
   const fetchUserInfo = async () => {
     const userInfo = await initialState?.fetchUserInfo?.();
@@ -52,11 +27,11 @@ const Login: React.FC = () => {
   const handleSubmit = async (values: API.LoginParams) => {
     try {
       // 登录
-      const msg = await login({ ...values, type });
+      const msg = await login({ ...values, type: 'account' });
       if (msg.status === 'ok') {
         const defaultLoginSuccessMessage = intl.formatMessage({
           id: 'pages.login.success',
-          defaultMessage: '登录成功！',
+          defaultMessage: 'Login successful!',
         });
         message.success(defaultLoginSuccessMessage);
         await fetchUserInfo();
@@ -69,14 +44,14 @@ const Login: React.FC = () => {
       setUserLoginState(msg);
     } catch (error) {
       const defaultLoginFailureMessage = intl.formatMessage({
-        id: 'pages.login.failure',
-        defaultMessage: '登录失败，请重试！',
+        id: 'pages.login.frontendErrorMessage',
+        defaultMessage: 'Login failed, please try again!',
       });
       console.log(error);
       message.error(defaultLoginFailureMessage);
     }
   };
-  const { status, type: loginType } = userLoginState;
+  const { status } = userLoginState;
 
   return (
     <AuthPage
@@ -103,26 +78,18 @@ const Login: React.FC = () => {
                 fontSize: 14,
               }}
             >
-              OR
+              {intl.formatMessage({
+                id: 'pages.login.or',
+                defaultMessage: 'OR',
+              })}
             </span>
           </Divider>
-          <Link to="/user/register">Register Now</Link>
-          {/* <Space align="center" size={24}>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                height: 40,
-                width: 40,
-                border: '1px solid ' + token.colorPrimaryBorder,
-                borderRadius: '50%',
-              }}
-            >
-              <AlipayOutlined style={{ ...iconStyles, color: '#1677FF' }} />
-            </div>
-          </Space> */}
+          <Link to="/user/register">
+            {intl.formatMessage({
+              id: 'pages.login.register',
+              defaultMessage: 'Register Now',
+            })}
+          </Link>
         </div>
       }
       onFinish={async (values) => {
@@ -130,180 +97,93 @@ const Login: React.FC = () => {
       }}
     >
       <Tabs
-        activeKey={type}
-        onChange={setType}
         centered
         items={[
           {
             key: 'account',
             label: intl.formatMessage({
-              id: 'pages.login.accountLogin.tab',
-              defaultMessage: '账户密码登录',
-            }),
-          },
-          {
-            key: 'mobile',
-            label: intl.formatMessage({
-              id: 'pages.login.phoneLogin.tab',
-              defaultMessage: '手机号登录',
+              id: 'pages.login.tab',
+              defaultMessage: 'Login',
             }),
           },
         ]}
       />
 
-      {status === 'error' && loginType === 'account' && (
+      {status === 'error' && (
         <ErrorMessage
           content={intl.formatMessage({
-            id: 'pages.login.accountLogin.errorMessage',
-            defaultMessage: '账户或密码错误(admin/ant.design)',
+            id: 'pages.login.backendErrorMessage',
+            defaultMessage: 'Incorrect email or password',
           })}
         />
       )}
-      {type === 'account' && (
-        <>
-          <ProFormText
-            name="username"
-            fieldProps={{
-              size: 'large',
-              prefix: <UserOutlined />,
-            }}
-            placeholder={intl.formatMessage({
-              id: 'pages.login.username.placeholder',
-              defaultMessage: '用户名: admin or user',
-            })}
-            rules={[
-              {
-                required: true,
-                message: (
-                  <FormattedMessage
-                    id="pages.login.username.required"
-                    defaultMessage="请输入用户名!"
-                  />
-                ),
-              },
-            ]}
-          />
-          <ProFormText.Password
-            name="password"
-            fieldProps={{
-              size: 'large',
-              prefix: <LockOutlined />,
-            }}
-            placeholder={intl.formatMessage({
-              id: 'pages.login.password.placeholder',
-              defaultMessage: '密码: ant.design',
-            })}
-            rules={[
-              {
-                required: true,
-                message: (
-                  <FormattedMessage
-                    id="pages.login.password.required"
-                    defaultMessage="请输入密码！"
-                  />
-                ),
-              },
-            ]}
-          />
-        </>
-      )}
 
-      {status === 'error' && loginType === 'mobile' && <ErrorMessage content="验证码错误" />}
-      {type === 'mobile' && (
-        <>
-          <ProFormText
-            fieldProps={{
-              size: 'large',
-              prefix: <MobileOutlined />,
-            }}
-            name="mobile"
-            placeholder={intl.formatMessage({
-              id: 'pages.login.phoneNumber.placeholder',
-              defaultMessage: '手机号',
-            })}
-            rules={[
-              {
-                required: true,
-                message: (
-                  <FormattedMessage
-                    id="pages.login.phoneNumber.required"
-                    defaultMessage="请输入手机号！"
-                  />
-                ),
-              },
-              {
-                pattern: /^1\d{10}$/,
-                message: (
-                  <FormattedMessage
-                    id="pages.login.phoneNumber.invalid"
-                    defaultMessage="手机号格式错误！"
-                  />
-                ),
-              },
-            ]}
-          />
-          <ProFormCaptcha
-            fieldProps={{
-              size: 'large',
-              prefix: <LockOutlined />,
-            }}
-            captchaProps={{
-              size: 'large',
-            }}
-            placeholder={intl.formatMessage({
-              id: 'pages.login.captcha.placeholder',
-              defaultMessage: '请输入验证码',
-            })}
-            captchaTextRender={(timing, count) => {
-              if (timing) {
-                return `${count} ${intl.formatMessage({
-                  id: 'pages.getCaptchaSecondText',
-                  defaultMessage: '获取验证码',
-                })}`;
-              }
-              return intl.formatMessage({
-                id: 'pages.login.phoneLogin.getVerificationCode',
-                defaultMessage: '获取验证码',
-              });
-            }}
-            name="captcha"
-            rules={[
-              {
-                required: true,
-                message: (
-                  <FormattedMessage
-                    id="pages.login.captcha.required"
-                    defaultMessage="请输入验证码！"
-                  />
-                ),
-              },
-            ]}
-            onGetCaptcha={async (phone) => {
-              const result = await getFakeCaptcha({
-                phone,
-              });
-              if (!result) {
-                return;
-              }
-              message.success('获取验证码成功！验证码为：1234');
-            }}
-          />
-        </>
-      )}
+      <ProFormText
+        name="email"
+        fieldProps={{
+          size: 'large',
+          prefix: <MailOutlined />,
+        }}
+        placeholder={intl.formatMessage({
+          id: 'pages.login.email.placeholder',
+          defaultMessage: 'Email',
+        })}
+        rules={[
+          {
+            required: true,
+            message: (
+              <FormattedMessage
+                id="pages.login.email.required"
+                defaultMessage="Please enter email!"
+              />
+            ),
+          },
+          {
+            // validate email format
+            type: 'email',
+            message: intl.formatMessage({
+              id: 'pages.login.email.invalid',
+              defaultMessage: 'Please enter a valid email address!',
+            }),
+          },
+        ]}
+      />
+      <ProFormText.Password
+        name="password"
+        fieldProps={{
+          size: 'large',
+          prefix: <LockOutlined />,
+        }}
+        placeholder={intl.formatMessage({
+          id: 'pages.login.password.placeholder',
+          defaultMessage: 'Password',
+        })}
+        rules={[
+          {
+            required: true,
+            message: (
+              <FormattedMessage
+                id="pages.login.password.required"
+                defaultMessage="Please enter password!"
+              />
+            ),
+          },
+        ]}
+      />
       <div
         style={{
           marginBottom: 24,
         }}
       >
         <ProFormCheckbox noStyle name="autoLogin">
-          <FormattedMessage id="pages.login.rememberMe" defaultMessage="自动登录" />
+          <FormattedMessage id="pages.login.rememberMe" defaultMessage="Remember me" />
         </ProFormCheckbox>
         <a
           style={{
             float: 'right',
           }}
         >
-          <FormattedMessage id="pages.login.forgotPassword" defaultMessage="忘记密码" />
+          <FormattedMessage id="pages.login.forgotPassword" defaultMessage="Forgot password?" />
         </a>
       </div>
     </AuthPage>
--- src/services/nanores-cloud/typings.d.ts
+++ src/services/nanores-cloud/typings.d.ts
@@ -104,7 +104,7 @@ declare namespace API {
   };
 
   type LoginParams = {
-    username?: string;
+    email?: string;
     password?: string;
     autoLogin?: boolean;
     type?: string;
