5213e8fa2e8e23ed814577570ca22eece280b873
Tao Zhou <taozhou@umich.edu>
2023-12-11 20:27:46 -0500

    feat: add creation modal form and actions in task page

--- mock/task.ts
+++ mock/task.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 const tasks = [
   {
-    id: 1,
+    task_id: 1,
     task_name: 'Task 1',
     task_description: 'Mapping the Distribution of Mitochondrial Proteins',
     create_time: new Date('2023-08-20T09:30:51.01'),
@@ -12,10 +12,10 @@ const tasks = [
     task_config_id: 2,
     total_cycles: 60,
     current_cycle: 23,
-    task_status: 'processing',
+    task_status: 'PROCESSING',
   },
   {
-    id: 2,
+    task_id: 2,
     task_name: 'Task 2',
     task_description: 'Synaptic Vesicle Proteins in Neurons',
     create_time: new Date('2023-07-15T11:45:23.17'),
@@ -25,10 +25,10 @@ const tasks = [
     task_config_id: 2,
     total_cycles: 60,
     current_cycle: 60,
-    task_status: 'success',
+    task_status: 'SUCCESS',
   },
   {
-    id: 3,
+    task_id: 3,
     task_name: 'Task 3',
     task_description: 'Membrane Protein Clustering in Bacteria',
     create_time: new Date('2023-10-02T16:37:52.19'),
@@ -38,10 +38,10 @@ const tasks = [
     task_config_id: 2,
     total_cycles: 60,
     current_cycle: 52,
-    task_status: 'processing',
+    task_status: 'PROCESSING',
   },
   {
-    id: 4,
+    task_id: 4,
     task_name: 'Task 4',
     task_description: 'draft',
     create_time: new Date('2023-10-05T16:37:52.19'),
@@ -51,11 +51,24 @@ const tasks = [
     task_config_id: 2,
     total_cycles: 60,
     current_cycle: 0,
-    task_status: 'waiting',
+    task_status: 'WAITING',
   },
   {
-    id: 5,
+    task_id: 5,
     task_name: 'Task 5',
+    task_description: 'Some Failed Task',
+    create_time: new Date('2023-10-05T16:37:52.19'),
+    update_time: new Date('2023-10-06T17:45:32.37'),
+    user_id: '1',
+    input_file_id: 5,
+    task_config_id: 2,
+    total_cycles: 60,
+    current_cycle: 50,
+    task_status: 'FAILED',
+  },
+  {
+    task_id: 6,
+    task_name: 'Task 6',
     task_description: 'Mapping the Distribution of Mitochondrial Proteins',
     create_time: new Date('2023-06-15T08:52:01.03'),
     update_time: new Date('2023-08-31T21:05:50.23'),
@@ -64,7 +77,7 @@ const tasks = [
     task_config_id: 9,
     total_cycles: 60,
     current_cycle: 2,
-    task_status: 'processing',
+    task_status: 'PROCESSING',
   },
 ];
 
--- src/pages/Task/index.tsx
+++ src/pages/Task/index.tsx
@@ -1,12 +1,141 @@
 import { getUserTasks } from '@/services/nanores-cloud/task';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import {
+  CaretRightOutlined,
+  CopyOutlined,
+  DownloadOutlined,
+  EditOutlined,
+  PauseCircleOutlined,
+  PauseOutlined,
+  PlayCircleOutlined,
+  RedoOutlined,
+} from '@ant-design/icons';
 import { PageContainer, ProList } from '@ant-design/pro-components';
-import { Card, List, Progress } from 'antd';
+import { Button, Card, List, Progress, Tooltip, message } from 'antd';
+import React, { useState } from 'react';
+import TaskForm from './components/ModalForm';
+
+function taskToActions({ task_status, task_id }: API.TaskItem) {
+  const statusToActions = new Map<API.TaskStatus, React.ReactNode[]>([
+    [
+      'WAITING',
+      [
+        <Tooltip title="Start" key="start">
+          <PlayCircleOutlined
+            onClick={() => {
+              message.success(`${task_id} started`);
+            }}
+          />
+        </Tooltip>,
+        <Tooltip title="Edit" key="edit">
+          <EditOutlined />
+        </Tooltip>,
+      ],
+    ],
+    [
+      'PROCESSING',
+      [
+        <Tooltip title="Pause" key="pause">
+          <PauseCircleOutlined />
+        </Tooltip>,
+      ],
+    ],
+    [
+      'SUCCESS',
+      [
+        <Tooltip title="Download" key="download">
+          <DownloadOutlined />
+        </Tooltip>,
+      ],
+    ],
+    [
+      'FAILED',
+      [
+        <Tooltip title="Retry" key="retry">
+          <RedoOutlined />
+        </Tooltip>,
+      ],
+    ],
+  ]);
+  let actions = statusToActions.get(task_status);
+
+  // common actions
+  actions = actions.concat(
+    <Tooltip title="Duplicate" key="duplicate">
+      <CopyOutlined />
+    </Tooltip>,
+  );
+  return actions;
+}
+
+const MainAction: React.FC<{ task: API.TaskItem; buttonSize: number }> = ({
+  // use nested destructuring
+  task: { task_status, task_id },
+  buttonSize,
+}) => {
+  const iconProps = { style: { fontSize: buttonSize / 2 } };
+  const statusToIcon = new Map<API.TaskStatus, React.ReactNode>([
+    [
+      'WAITING',
+      <CaretRightOutlined
+        key="start"
+        {...iconProps}
+        onClick={() => {
+          message.success(`${task_id} started`);
+        }}
+      />,
+    ],
+    ['PROCESSING', <PauseOutlined key="pause" {...iconProps} />],
+    ['SUCCESS', <DownloadOutlined key="download" {...iconProps} />],
+    ['FAILED', <RedoOutlined key="retry" {...iconProps} />],
+  ]);
+
+  return (
+    <Button
+      style={{ marginTop: '20px', width: buttonSize, height: buttonSize }}
+      shape="circle"
+      icon={statusToIcon.get(task_status)}
+    />
+  );
+};
+
+const ProgressButton: React.FC<{
+  task: API.TaskItem;
+}> = ({ task }) => {
+  const { task_status, current_cycle, total_cycles } = task;
+  const buttonSize = 100;
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div
+      onMouseOver={() => {
+        setIsHovered(true);
+      }}
+      // not called consistently due to react's bug
+      onMouseOut={() => {
+        setIsHovered(false);
+      }}
+    >
+      {isHovered ? (
+        <MainAction task={task} buttonSize={buttonSize} />
+      ) : (
+        <Progress
+          style={{ marginTop: '20px' }}
+          size={buttonSize}
+          type="circle"
+          status={task_status === 'FAILED' ? 'exception' : undefined}
+          percent={Math.round((current_cycle / total_cycles) * 100)}
+        />
+      )}
+    </div>
+  );
+};
 
 const Task: React.FC = () => {
   return (
     <PageContainer>
       <ProList<API.TaskItem>
+        toolbar={{
+          actions: [<TaskForm key="create" />],
+        }}
         // from antd List
         renderItem={(task) => (
           <List.Item>
@@ -17,18 +146,8 @@ const Task: React.FC = () => {
                 flexDirection: 'column',
                 alignItems: 'center',
               }}
-              cover={
-                <Progress
-                  style={{ marginTop: '20px' }}
-                  type="circle"
-                  percent={Math.round((task.current_cycle / task.total_cycles) * 100)}
-                />
-              }
-              actions={[
-                <SettingOutlined key="setting" />,
-                <EditOutlined key="edit" />,
-                <EllipsisOutlined key="ellipsis" />,
-              ]}
+              cover={<ProgressButton task={task} />}
+              actions={taskToActions(task)}
             >
               <Card.Meta
                 style={{ textAlign: 'center' }}
